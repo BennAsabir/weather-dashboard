bfc6ff6d4df0e9b8a08eebabb2ad9f472710196f
Benzino <benasabir@gmail.com>
2021-01-15 16:55:59 -0500

    updated js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var citySearch;
+var APIkey = '&appid=c8476d823b852459171f57cf8c8f09f2';
+var weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
+var uviAPI = 'https://api.openweathermap.org/data/2.5/uvi?lat=';
+var forecastAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+var units = '&units=metric';
+var getWeatherIcon = 'http://openweathermap.org/img/wn/';
+var searchHistoryArr = [];
